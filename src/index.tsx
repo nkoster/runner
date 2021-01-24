67359979113bfe2b7e07d5288b6422ab709ad747
Niels Koster <n.koster@portavita.eu>
2021-01-24 22:09:56 +0100

    Use define to substitute some values like process.env.NODE_ENV and global

--- src/index.tsx
+++ src/index.tsx
@@ -22,7 +22,11 @@ const App = () => {
             entryPoints: ['index.js'],
             bundle: true,
             write: false,
-            plugins: [unpkgPathPlugin()]
+            plugins: [unpkgPathPlugin()],
+            define: {
+                'process.env.NODE_ENV': '"production"',
+                global: 'window'
+            }
         })
         // console.log('RESULT', result)
         setCode(result.outputFiles[0].text)
