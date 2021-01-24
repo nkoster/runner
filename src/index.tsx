3ef5bd9aa94874ae5c2833706f00654b9b213802
Niels Koster <n.koster@portavita.eu>
2021-01-24 19:08:01 +0100

    Use esbuild plugin

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,7 @@
 import * as esbuild from 'esbuild-wasm'
 import { useState, useEffect, useRef } from 'react'
 import ReactDOM from 'react-dom'
+import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 
 const App = () => {
     const ref = useRef<any>()
@@ -17,11 +18,14 @@ const App = () => {
     }, [])
     const onClick = async () => {
         if (!ref.current) return
-        const result = await ref.current.transform(input, {
-            loader: 'jsx',
-            target: 'es2015'
+        const result = await ref.current.build({
+            entryPoints: ['index.js'],
+            bundle: true,
+            write: false,
+            plugins: [unpkgPathPlugin()]
         })
-        setCode(result.code)
+        // console.log('RESULT', result)
+        setCode(result.outputFiles[0].text)
     }
     return (
         <div>
