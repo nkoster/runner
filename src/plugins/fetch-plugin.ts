d101095ba014fbefb3bd8f0c1e543be025783af9
Niels Koster <n.koster@portavita.eu>
2021-01-25 23:49:45 +0100

    Refactor: Use extra build.onLoad for the cache to prevent for duplicated code

--- src/plugins/fetch-plugin.ts
+++ src/plugins/fetch-plugin.ts
@@ -18,11 +18,15 @@ export const fetchPlugin = (inputCode: string) => {
             }
         })
 
-        build.onLoad({ filter: /\.css$/ }, async (args: any) => {
+        build.onLoad({ filter: /.*/ }, async (args: any) => {
             const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
             if (cachedResult) {
               return cachedResult
             }
+            // return null
+        })
+
+        build.onLoad({ filter: /\.css$/ }, async (args: any) => {
             const { data, request } = await axios.get(args.path)
             const escaped = data
             .replace(/\n/g, '')
@@ -42,10 +46,6 @@ export const fetchPlugin = (inputCode: string) => {
         })
 
         build.onLoad({ filter: /.*/ }, async (args: any) => {
-            const cachedResult = await fileCache.getItem<esbuild.OnLoadResult>(args.path)
-            if (cachedResult) {
-              return cachedResult
-            }
             const { data, request } = await axios.get(args.path)
             const result: esbuild.OnLoadResult = {
                 loader: 'jsx',
