3b8e1d6dcb6e7eba667383d357c801f94254609f
Niels Koster <n.koster@portavita.eu>
2021-01-29 10:59:37 +0100

    CodeEditor interface and props + useRef

--- src/components/code-editor.tsx
+++ src/components/code-editor.tsx
@@ -1,10 +1,39 @@
 import MonacoEditor from '@monaco-editor/react'
+import { useRef } from 'react'
 
-const CodeEditor = () => {
+interface CodeEditorProps {
+    initialValue?: string
+    onChange(value: string): void
+}
+
+const CodeEditor: React.FC<CodeEditorProps> = ({ initialValue = '', onChange }) => {
+    const editorRef = useRef<any>()
+
+    const change = () => {
+        onChange(editorRef.current.getValue())
+    }
+
+    function handleEditorDidMount(editor:any, monaco:any) {
+        editorRef.current = editor; 
+    }
+
     return <MonacoEditor
+        onChange={change}
+        onMount={handleEditorDidMount}
+        value={initialValue}
         height='500px'
         language='javascript'
         theme='vs-dark'
+        options={{
+            wordWrap: 'on',
+            minimap: { enabled: false },
+            showUnused: false,
+            folding: false,
+            lineNumbersMinChars: 3,
+            fontSize: 20,
+            scrollBeyondLastLine: false,
+            automaticLayout: true
+        }}
     />
 }
 
