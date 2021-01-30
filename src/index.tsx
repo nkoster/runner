402cd6f13495a9f09173e9cd1ece0ccde7c957b3
Niels Koster <n.koster@portavita.eu>
2021-01-31 00:13:34 +0100

    Refactor: move editor and preview to own component

--- src/index.tsx
+++ src/index.tsx
@@ -1,30 +1,12 @@
 import 'bulmaswatch/superhero/bulmaswatch.min.css'
-import { useState } from 'react'
 import ReactDOM from 'react-dom'
-import CodeEditor from './components/code-editor'
-import Preview from './components/preview'
-import { bundle } from './bundler'
+import CodeCell from './components/code-cell'
 
 const App = () => {
-
-    const [input, setInput] = useState('')
-    const [code, setCode] = useState('')
-
-    const onClick = async () => {
-        const output = await bundle(input)
-        setCode(output)
-    }
-
     return (
         <div>
-            <CodeEditor
-                initialValue=''
-                onChange={setInput}
-            />
-            <div>
-                <button onClick={onClick}>transpile</button>
-            </div>
-            <Preview code={code} />
+            <CodeCell />
+            <CodeCell />
         </div>
     )
 }
