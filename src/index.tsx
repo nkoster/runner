27315e9e3421834b2efbd3c02915b278f04b7ee3
Niels Koster <n.koster@portavita.eu>
2021-01-30 22:43:45 +0100

    Refactor: move iframe to preview component

--- src/index.tsx
+++ src/index.tsx
@@ -5,13 +5,14 @@ import ReactDOM from 'react-dom'
 import { unpkgPathPlugin } from './plugins/unpkg-path-plugin'
 import { fetchPlugin } from './plugins/fetch-plugin'
 import CodeEditor from './components/code-editor'
+import Preview from './components/preview'
 
 const App = () => {
 
     const ref = useRef<any>()
-    const iframe = useRef<any>()
 
     const [input, setInput] = useState('')
+    const [code, setCode] = useState('')
 
     const startService = async () => {
         ref.current = await esbuild.startService({
@@ -27,7 +28,6 @@ const App = () => {
     const onClick = async () => {
         if (!ref.current) return
         /* Reset the iframe, first */
-        iframe.current.srcdoc = html
         const result = await ref.current.build({
             entryPoints: ['index.js'],
             bundle: true,
@@ -38,51 +38,19 @@ const App = () => {
                 global: 'window'
             }
         })
-        iframe.current.contentWindow.postMessage(result.outputFiles[0].text, '*')
+        setCode(result.outputFiles[0].text)
     }
 
-    const html = `
-        <html>
-            <head></head>
-            <body>
-                <div id="root"></div>
-                <script>
-                    window.addEventListener('message', evt => {
-                        try {
-                            eval(evt.data)
-                        } catch(err) {
-                            const root = document.querySelector('#root')
-                            root.innerHTML = '<div style="color:red"><h4>Runner Error</h4>&nbsp;' + err + '</div>'
-                            throw err
-                        }
-                    }, false)
-                </script>
-            </body>
-        </html>
-    `
     return (
         <div>
             <CodeEditor
                 initialValue=''
                 onChange={setInput}
             />
-            <textarea
-                spellCheck={false}
-                value={input}
-                onChange={evt => { 
-                    // onClick(evt.target.value)
-                    setInput(evt.target.value)
-                }}
-            ></textarea>
             <div>
                 <button onClick={onClick}>transpile</button>
             </div>
-            <iframe
-                ref={iframe}
-                title='runner'
-                sandbox='allow-scripts'
-                srcDoc={html}
-            ></iframe>
+            <Preview code={code} />
         </div>
     )
 }
