0c6d93884833797c2b38e6017c885f92726aa6d6
Niels Koster <n.koster@portavita.eu>
2021-02-01 00:28:12 +0100

    Handle width dimensions + use debounce logic

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -9,26 +9,39 @@ interface ResizableProps {
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
     const [innerWidth, setInnerWidth] = useState(window.innerWidth)
     const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+    const [width, setWidth] = useState(window.innerWidth * .75)
     useEffect(() => {
+        /* Debounce, to limit updates during window resizing */
+        let timer: any
         const listener = () => {
-            setInnerWidth(window.innerWidth)
-            setInnerHeight(window.innerHeight)
+            if (timer) clearTimeout(timer)
+            timer = setTimeout(() => {
+                setInnerWidth(window.innerWidth)
+                setInnerHeight(window.innerHeight)
+                if (window.innerWidth * .75 < width) {
+                    setWidth(window.innerWidth * .75)
+                }
+            }, 100)
         }
         window.addEventListener('resize', listener)
         return () => {
             window.removeEventListener('resize', listener)
         }
     }
-    ,[])
+    ,[width])
     let resizableProps:ResizableBoxProps
     if (direction === 'horizontal') {
         resizableProps = {
             className: 'resize-horizontal',
             height: Infinity,
-            width: innerWidth * 0.9,
+            width,
             resizeHandles: ['e'],
             minConstraints: [innerWidth - (innerWidth * 0.9), Infinity],
-            maxConstraints: [innerWidth * 0.9, Infinity]        }
+            maxConstraints: [innerWidth * 0.9, Infinity],
+            onResizeStop: (evt, data) => {
+                setWidth(data.size.width)
+            }
+        }
     } else {
         resizableProps = {
             height: 300,
