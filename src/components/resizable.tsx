7e48bdadf0d14233d6abe0fc6f274d904b03429d
Niels Koster <n.koster@portavita.eu>
2021-01-31 22:53:55 +0100

    Update dimensions when window size changes

--- src/components/resizable.tsx
+++ src/components/resizable.tsx
@@ -1,4 +1,5 @@
 import './resizable.css'
+import { useEffect, useState } from 'react'
 import { ResizableBox, ResizableBoxProps } from 'react-resizable'
 
 interface ResizableProps {
@@ -6,22 +7,35 @@ interface ResizableProps {
 }
 
 const Resizable: React.FC<ResizableProps> = ({ direction, children }) => {
+    const [innerWidth, setInnerWidth] = useState(window.innerWidth)
+    const [innerHeight, setInnerHeight] = useState(window.innerHeight)
+    useEffect(() => {
+        const listener = () => {
+            setInnerWidth(window.innerWidth)
+            setInnerHeight(window.innerHeight)
+        }
+        window.addEventListener('resize', listener)
+        return () => {
+            window.removeEventListener('resize', listener)
+        }
+    }
+    ,[])
     let resizableProps:ResizableBoxProps
     if (direction === 'horizontal') {
         resizableProps = {
             className: 'resize-horizontal',
             height: Infinity,
-            width: window.innerWidth * 0.9,
+            width: innerWidth * 0.9,
             resizeHandles: ['e'],
-            minConstraints: [window.innerWidth - (window.innerWidth * 0.9), Infinity],
-            maxConstraints: [window.innerWidth * 0.9, Infinity]        }
+            minConstraints: [innerWidth - (innerWidth * 0.9), Infinity],
+            maxConstraints: [innerWidth * 0.9, Infinity]        }
     } else {
         resizableProps = {
             height: 300,
             width: Infinity,
             resizeHandles: ['s'],
-            minConstraints: [Infinity, window.innerHeight - (window.innerHeight * 0.9)],
-            maxConstraints: [Infinity, window.innerHeight * 0.9]
+            minConstraints: [Infinity, innerHeight - (innerHeight * 0.9)],
+            maxConstraints: [Infinity, innerHeight * 0.9]
         }        
     }
     return (
