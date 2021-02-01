7d9f5f3c86705507d2ef96918271de209c90d324
Niels Koster <n.koster@portavita.eu>
2021-02-01 21:36:51 +0100

    Add hooks to click and switch between editor and viewer mode

--- src/components/text-editor.tsx
+++ src/components/text-editor.tsx
@@ -1,17 +1,35 @@
 import MDEditor from '@uiw/react-md-editor'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 
 const TextEditor:React.FC = () => {
-    const [value, setValue] = useState('aap')
+
+    const [value, setValue] = useState('# Hi!')
+    const [editing, setEditing] = useState(false)
+
     const updateValue = (value:any) => {
         setValue(value)
     }
-    return (
-        <div>
+
+    useEffect(() => {
+        const listener = () => {
+            setEditing(false)
+        }
+        document.addEventListener('click', listener, { capture: true })
+        return () => {
+            document.removeEventListener('click', listener, { capture: true })
+        }
+    }, [])
+
+    if (editing) {
+        return (
             <MDEditor
                 value={value}
                 onChange={updateValue}
             />
+        )
+    }
+    return (
+        <div onClick={() => setEditing(true)}>
             <MDEditor.Markdown source={value} />
         </div>
     )
