e25044d7ccbe725ee9f951f641391d91870b5bd5
Niels Koster <n.koster@portavita.eu>
2021-02-01 19:19:26 +0100

    Handle async errors, too, in the bundle

--- src/components/preview.tsx
+++ src/components/preview.tsx
@@ -15,13 +15,20 @@ const html = `
     <body>
         <div id="root"></div>
         <script>
+            const handleError = err => {
+                const root = document.querySelector('#root')
+                root.innerHTML = '<div style="color:red"><h4>Runner Error</h4>&nbsp;' + err + '</div>'
+                throw err
+            }
+            window.addEventListener('error', evt => {
+                evt.preventDefault()
+                handleError(evt.error)
+            }, false)
             window.addEventListener('message', evt => {
                 try {
                     eval(evt.data)
                 } catch(err) {
-                    const root = document.querySelector('#root')
-                    root.innerHTML = '<div style="color:red"><h4>Runner Error</h4>&nbsp;' + err + '</div>'
-                    throw err
+                    handleError(err)
                 }
             }, false)
         </script>
@@ -35,7 +42,7 @@ const Preview: React.FC<PreviewProps> = ({ code }) => {
         iframe.current.srcdoc = html
         setTimeout(() => {
             iframe.current.contentWindow.postMessage(code, '*')
-        }, 50)
+        }, 100)
     }, [code])
     return (
         <div className='preview-wrapper'>
