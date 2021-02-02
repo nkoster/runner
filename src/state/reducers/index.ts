322cf1f86972719c79c688ef3ccd7c9bd077142b
Niels Koster <n.koster@portavita.eu>
2021-02-02 22:59:51 +0100

    More redux scaffolding

--- src/state/reducers/index.ts
+++ src/state/reducers/index.ts
@@ -1,1 +1,10 @@
-export const placeholder = () => {}
+import { combineReducers } from 'redux'
+import CellReducer from './cellReducer'
+
+const reducers = combineReducers({
+    cells: CellReducer
+})
+
+export default reducers
+
+export type RootState = ReturnType<typeof reducers>
