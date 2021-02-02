fad63e8fdd54d246d7802e2f856b33cc2e781ead
Niels Koster <n.koster@portavita.eu>
2021-02-03 00:14:08 +0100

    Action creator scaffolding

--- src/state/action-creators/index.ts
+++ src/state/action-creators/index.ts
@@ -1,1 +1,46 @@
-export const placeholder = () => {}
+import { ActionTypes } from '../action-types'
+import { CellTypes } from '../cell'
+import { 
+    Actions,
+    MoveCellAction,
+    InsertCellAction,
+    DeleteCellAction,
+    UpdateCellAction
+} from '../actions'
+
+export const updateCell = (id: string, content: string): UpdateCellAction => {
+    return {
+        type: ActionTypes.UPDATE_CELL,
+        payload: {
+            id,
+            content
+        }
+    }
+}
+
+export const moveCell = (id: string, direction: 'up' | 'down'): MoveCellAction => {
+    return {
+        type: ActionTypes.MOVE_CELL,
+        payload: {
+            id,
+            direction
+        }
+    }
+}
+
+export const insertCell = (id: string, type: CellTypes): InsertCellAction => {
+    return {
+        type: ActionTypes.INSERT_CELL,
+        payload: {
+            id,
+            type
+        }
+    }
+}
+
+export const deleteCell = (id: string): DeleteCellAction => {
+    return {
+        type: ActionTypes.DELETE_CELL,
+        payload: id
+    }
+}
